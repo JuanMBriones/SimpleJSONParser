822d1f238ad1661062e2d607528f36c385b9b5c9
Juan Briones <JuanMBriones@outlook.com>
2020-05-22 15:58:56 -0600

    Initial Commit

---
+++ SmartBike/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SmartBike
+//
+//  Created by Juan Briones on 22/05/20.
+//  Copyright © 2020 Juan Briones. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
